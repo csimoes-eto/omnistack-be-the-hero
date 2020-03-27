c453850701501fc6c1c352484f76b2229b653a1e
Carlos Simões <csimoes10@gmail.com>
2020-03-27 16:01:15 +0000

    backend tests

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 
@@ -11,9 +11,9 @@ module.exports = {
     },
 
     async create(request, response) {
-        const { name, email, whatsapp, city, uf } = request.body;
+        const { name, email, whatsapp, city } = request.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
